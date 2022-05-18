3e0f34bfb4a695992ea07e487b0d0a80acc9c238
champer3 <okunolas21@gmail.com>
2022-05-18 00:37:05 -0700

    read and writing in nodejs

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const name = "Hello World"
+// const name = "Hello World"
+// console.log(name)
 
-console.log(name)
+// Blocking, synchronous way
+
 const fs = require("fs")
-console.log(fs)
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+
+
+const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`
+
+fs.writeFileSync('./txt/output.txt', textOut)
+
+console.log("file created")
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>{
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>{
+        console.log(data2)
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) =>{
+            console.log(data3)
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err =>{
+                console.log("your file has been writen")
+            })
+        })
+    })
+})
