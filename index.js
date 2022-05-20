da852e42b5aff58242a9f51bb7f36e747f4e0c56
champer3 <okunolas21@gmail.com>
2022-05-19 17:46:45 -0700

    adding modules

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const http = require('http')
 const { json } = require("node:stream/consumers")
 const url = require('url')
 
+const replaceTemplate = require('./modules/replaceTemplate')
+
 ////////////////////////////////////////////////
 //////FILES
 
@@ -35,20 +37,6 @@ const url = require('url')
 
 ////////////////////////////////////////////////
 //////SERVER
-const replaceTemplate = (temp, product) =>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%ID%}/g, product.id)
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    return output
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
