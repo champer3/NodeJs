3376a9ae31630d1a2b93e759c5c411f4c9597cbb
champer3 <okunolas21@gmail.com>
2022-05-28 09:00:46 -0700

    learning how to fetch APIs

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -3,10 +3,30 @@ const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`breed: ${data}`);
+  //   Callback Hell
+  //   superagent
+  //     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+  //     .end((err, res) => {
+  //       if (err) return console.log(err.message);
+  //       console.log(res.body.message);
 
+  //       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+  //         if (err) return console.log(err.message);
+  //         console.log('Random image saved to rew file');
+  //       });
+  //     });
+
+  //Promises
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+    .then((res) => {
       console.log(res.body.message);
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random image saved to rew file');
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
