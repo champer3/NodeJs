a7cf7d9cc655942df415ebea08182f177bbcd56f
champer3 <okunolas21@gmail.com>
2022-06-03 23:09:18 -0700

    removed uneccessary commit

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -2,8 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-
-// console.log(process.env);
+// const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
