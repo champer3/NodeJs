7a51c45a862e5f592b4e37f3c1d68a1b938e63ba
champer3 <okunolas21@gmail.com>
2022-06-05 13:50:42 -0700

    Creating new tours with MongoDb

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,19 +1,22 @@
+/* eslint-disable prettier/prettier */
 const express = require('express');
 const morgan = require('morgan');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARE
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development')
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
+  console.log('Hello from the middleware 👋');
   next();
 });
 
@@ -23,7 +26,6 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
