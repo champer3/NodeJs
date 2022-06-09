898d337865fbecbd9dae651a7eba2acdfdaf5c3a
champer3 <okunolas21@gmail.com>
2022-06-09 11:07:15 +0100

    learning built-in and custom data validators

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -64,7 +64,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'failed',
-      message: 'invalid data sent',
+      message: err,
     });
   }
 };
--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -29,10 +29,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -42,7 +48,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          //This only points to current document on New Doc Creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
