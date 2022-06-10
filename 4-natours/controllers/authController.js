2537870bd659668c2828850bb876dc105a91c585
champer3 <okunolas21@gmail.com>
2022-06-10 11:51:20 +0100

    Authentication and authorization procedure

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,12 +11,34 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1. Check if email and password exiss
+  if (!email || !password) {
+    return next(new AppError('Please provide email and passowrd', 400));
+  }
+
+  // 2. Check if user exist && password is correct
+  const user = User.findOne({ email });
+
+  // 3. If everything is Okay, send token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
